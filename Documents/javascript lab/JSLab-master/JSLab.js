ed6beb73c1e80abd8e93fd8e33f772c5d0cf8054
linde593 <linde593@morris.umn.edu>
2014-02-03 15:16:57 -0600

    Initial commit

---
+++ Documents/javascript lab/JSLab-master/JSLab.js
@@ -0,0 +1,70 @@
+/**
+ * Created by gensc004 on 1/28/14.
+ */
+//
+
+function eraseText() {
+    document.getElementById("textArea").value = "";
+}
+
+function displayCharacterCount(text){
+    var count = "<p>" +"Count: "  + text.length + "</p>";
+    return count;
+}
+
+var save = 0;
+var currentSave;
+var currentSave1;
+var currentSave2;
+var currentSave3;
+var currentSave4;
+
+function save(text) {
+
+
+    if (saveCounter == 0) {
+        currentSave = text;
+        saveCounter++;
+    } else if (saveCounter == 1) {
+        currentSave1 = text;
+        saveCounter++;
+    } else if (saveCounter == 2) {
+        currentSave2 = text;
+        saveCounter++;
+    } else if (saveCounter == 3) {
+        currentSave3 = text;
+        saveCounter++;
+    } else if (saveCounter == 4) {
+        currentSave4 = text;
+        saveCounter++;
+    }
+
+    return function undo(newText) {
+        //var save = "hello";
+        if (saveCounter == 1) {
+            save = currentSave;
+            saveCounter--;
+        } else if (saveCounter == 2) {
+            save = currentSave1;
+            saveCounter--;
+        } else if (saveCounter == 3) {
+            save = currentSave2;
+            saveCounter--;
+        } else if (saveCounter == 4) {
+            save = currentSave3;
+            saveCounter--;
+        } else if (saveCounter == 5) {
+            save = currentSave4;
+            saveCounter--;
+        }
+
+        return save;
+    }
+
+}
+
+
+
+
+module.exports.eraseText = eraseText;
+module.exports.displayCharacterCount = displayCharacterCount;
